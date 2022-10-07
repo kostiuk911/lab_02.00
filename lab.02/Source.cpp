6ea05b9b02bbc3f4ea46c061f8b8c41ae75d4ecc
kostiuk911 <kostiukvitaliy043@gmail.com>
2022-10-07 20:29:06 +0300

    first branch_02 commit

--- lab.02/Source.cpp
+++ lab.02/Source.cpp
@@ -9,12 +9,12 @@
 using namespace std;
 int main() {
     double a;
-    double z1;
+    // double z1;
     double z2;
     cout << "a= "; cin >> a; cout << endl;
-    z1 = sin((M_PI / 2.) + (3 * a)) / (1 - sin(3 * a - M_PI));
+    // z1 = sin((M_PI / 2.) + (3 * a)) / (1 - sin(3 * a - M_PI));
     z2 = (cos(1.25 * M_PI + 1.5 * a)) / (sin(1.25 * M_PI + 1.5 * a));
-    cout << "z1= " << z1 << endl;
+    // cout << "z1= " << z1 << endl;
     cout << "z2= " << z2 << endl;
 
     return 0;
